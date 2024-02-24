474066b567eef02f1a3c25b197dbd9b6605bd0c4
Patricia McPhee <hi@patriciamcphee.com>
2024-02-24 12:41:09 -0800

    fix footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const MyFooter = () => (
   <Footer className={styles.footerContainer}>
     <Row align="top">
     <Col span={24}>
-        <div className={styles.logo}><img src="/logo-white-full.png" alt="logo white" height="180px" /></div>
+        <p className={styles.footerBody}><img className={styles.logo} src="/logo-white-full.png" alt="logo white" /></p>
 
         <p className={styles.footerBody}>
         (206) 880-0886 | <a href="mailto:chris@leaflinegarden.deaign">chris@leaflinegarden.deaign</a></p>
